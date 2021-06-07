e38c14c150da3411d29ad0b437934cedea4dc43a
Vaishnavi Ambolkar <55630110+VaishnaviAmbolkar@users.noreply.github.com>
2021-06-07 11:49:21 +0530

    Create reg.vue

---
+++ reg.vue
@@ -0,0 +1,27 @@
+<script >
+   var app = new Vue({
+     el: '#app',
+     data: {
+           step:1,
+           fname:'',
+           lname:'',
+           address1:'',
+           address2:'',
+           City:'',
+           State:'',
+           Zipcode:'',
+           email:'',
+       },
+   methods:{
+       prev() {
+           this.step--;
+       },
+       next() {
+           this.step++;
+       },  
+       submit() {
+           alert('Form Is Submitted.');      
+       }   
+   }  
+    })
+</script>
